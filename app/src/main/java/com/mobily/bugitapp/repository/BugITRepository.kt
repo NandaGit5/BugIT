41ca96d8cb255954be0914ee6b56871a9bb1d97f
nandae <nandae.in@mouritech.com>
2024-10-28 17:38:22 +0530

    Updated code with json file and repo changes

--- app/src/main/java/com/mobily/bugitapp/repository/BugITRepository.kt
+++ app/src/main/java/com/mobily/bugitapp/repository/BugITRepository.kt
@@ -1,7 +1,7 @@
 package com.mobily.bugitapp.repository
 
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.android.volley.DefaultRetryPolicy
-import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
@@ -9,17 +9,21 @@ import com.mobily.bugitapp.apis.BugITAPI
 import com.mobily.bugitapp.utilities.AppConstants.APP_SCRIPT_WEB_APP_URL
 import javax.inject.Inject
 
+
+
 class BugITRepository @Inject constructor(private val requestQueue: RequestQueue) : BugITAPI {
     override fun submitBugReport(
         action: String,                         // The action to perform (e.g., insert, update)
         userId: String,                         // The user ID associated with the bug report
         userName: String,                       // The user name associated with the bug report
-        userImage: String,                      // The image associated with the bug report (in string format)
-        listener: Response.Listener<String>,    // Listener for successful response
-        errorListener: Response.ErrorListener   // Listener for error response
+        userImage: String, // The image associated with the bug report (in string format)
+        selectedImageUris: SnapshotStateList<String>,    // Listener for successful response
+        listener: Response.Listener<String>,
+        errorListener: Response.ErrorListener   // Listener for error response){}
     ): StringRequest {
-        val url = APP_SCRIPT_WEB_APP_URL                  // URL of the Google Apps Script web app
-        val stringRequest = object : StringRequest(Request.Method.POST, url, listener, errorListener) {
+        val url = APP_SCRIPT_WEB_APP_URL
+        // URL of the Google Apps Script web app
+        val stringRequest = object : StringRequest(Method.POST, url, listener, errorListener) {
             override fun getParams(): MutableMap<String, String> {
                 val params = HashMap<String, String>()
                 params["action"] = action
